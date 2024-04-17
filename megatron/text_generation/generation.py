137208ed0b2d0702373489f7292595cff090367b
Dongximing <dongx34@myumanitoba.ca>
2024-04-17 15:52:22 -0400

    add log

--- megatron/text_generation/generation.py
+++ megatron/text_generation/generation.py
@@ -284,11 +284,12 @@ def generate_tokens_probs_and_return_on_first_stage(
 
     generated_sequence_lengths = broadcast_from_last_to_first_pipeline_stage(
         batch_size, torch.int64, generated_sequence_lengths)
+    print("output_log_probs--------> in 287",output_log_probs)
     if return_output_log_probs:
         output_log_probs_size = (batch_size, context_length)
         output_log_probs = broadcast_from_last_to_first_pipeline_stage(
             output_log_probs_size, torch.float32, output_log_probs)
-    print("output_log_probs in 285", output_log_probs)
+    print("output_log_probs in 292", output_log_probs)
 
     return tokens, generated_sequence_lengths, output_log_probs, None
 
