6b5a3c54e78f5f09fe759433b7b2ef07260ce3fd
Dongximing <dongx34@myumanitoba.ca>
2024-04-17 16:47:58 -0400

    adding candidate

--- megatron/text_generation/api.py
+++ megatron/text_generation/api.py
@@ -77,10 +77,11 @@ def generate_and_post_process(model,
     return None
 def featureAblation(original_input,add_BOS):
     length = original_input.size(1)
+
     result = []
     for i in range(length):
         new_tensor = original_input.clone()
-        if new_tensor[:,i] == add_BOS:
+        if new_tensor[:,i] == 50256:
             break
         new_tensor[:,i] = 0
         result.append(new_tensor)
